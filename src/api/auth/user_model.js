fbe4e34c77a53de40452ef41bf50212a989da096
Peter Vertesi <vpeter1119@gmail.com>
2020-07-07 11:07:04 +0200

    Add user model (authentication data)

---
+++ src/api/auth/user_model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+//const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
+
+userSchema.plugin(uniqueValidator);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
